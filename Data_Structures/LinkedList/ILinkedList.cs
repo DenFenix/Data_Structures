0cd13e43b583cce3e101403e2ea502e013a0294d
DM <Denis.m.a.3@gmail.com>
2020-08-04 21:15:21 +0300

    Реализован связынй список, и частично динамический массив

---
+++ Data_Structures/LinkedList/ILinkedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures.LinkedList
+{
+    interface ILinkedList<T>
+    {
+        T NextValue();
+    }
+}
---
+++ Data_Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
