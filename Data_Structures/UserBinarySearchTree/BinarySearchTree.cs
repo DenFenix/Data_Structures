dbf144a91d62d4ea60d28887a7c6494de33be813
DM <Denis.m.a.3@gmail.com>
2020-08-07 22:15:59 +0300

    Заготовка для двоичного дерева поиска

---
+++ Data_Structures/UserBinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures.UserBinarySearchTree
+{
+    ///У каждого узла не более двух детей.
+    ///Любое значение меньше значения узла становится 
+    ///левым ребенком или ребенком левого ребенка.
+    ///Любое значение больше или равное значению узла становится 
+    ///правым ребенком или ребенком правого ребенка.
+    class BinarySearchTree
+    {
+    }
+}
